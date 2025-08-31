b8cad9c0560375a2f673733cc3c3aeff032d7770
Steven <erewhon@flatland.org>
2025-08-31 01:49:21 -0500

    experimenting with CRT effect

--- ui/app/demo/page.tsx
+++ ui/app/demo/page.tsx
@@ -1,14 +1,27 @@
 import CrtMonitor from "@/components/CrtMonitor"
+import TelescopeControl from "@/telescope-control"
 
 export default function CrtDemo() {
   return (
+
     <div className="min-h-screen bg-neutral-900 text-neutral-100 flex items-center justify-center p-10">
       <CrtMonitor width={1024} height={720} bezelLabel="VINTAGE-3200" curvature={0.15}>
         <div className="w-[1024px] h-[720] flex flex-col items-center justify-center bg-black text-amber-300 font-mono">
-          <div className="text-5xl mb-6">HELLO, WORLD_</div>
-          <div className="opacity-80">Press any key to continue…</div>
+          {/*<div className="text-5xl mb-6">HELLO, WORLD_</div>*/}
+          {/*<div className="opacity-80">Press any key to continue…</div>*/}
+          <TelescopeControl/>
         </div>
       </CrtMonitor>
     </div>
   )
+  // return (
+  //   <div className="min-h-screen bg-neutral-900 text-neutral-100 flex items-center justify-center p-10">
+  //     <CrtMonitor width={1024} height={720} bezelLabel="VINTAGE-3200" curvature={0.15}>
+  //       <div className="w-[1024px] h-[720] flex flex-col items-center justify-center bg-black text-amber-300 font-mono">
+  //         <div className="text-5xl mb-6">HELLO, WORLD_</div>
+  //         <div className="opacity-80">Press any key to continue…</div>
+  //       </div>
+  //     </CrtMonitor>
+  //   </div>
+  // )
 };
